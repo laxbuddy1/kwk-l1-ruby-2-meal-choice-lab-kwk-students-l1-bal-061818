0778590dbb3dad7dc76592c3f4c08ead20ed6037
laxbuddy1 <jcyachimowicz131@gmail.com>
2018-06-19 16:01:51 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,4 +2,17 @@ def breakfast(food="eggs")
   puts "For breakfast I love #{food}"
 end 
 
-breakfast("frosted flakes")
+breakfast("frosted flakes") 
+
+
+def lunch(food="hot dogs") 
+  puts "For lunch I love #{food}" 
+end 
+
+lunch("grilled cheese") 
+
+
+def dinner(food="soup") 
+  puts "For dinner I love #{food}"
+end
+
